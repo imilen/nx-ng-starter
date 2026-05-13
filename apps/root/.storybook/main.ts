9caef45433d95172aecf9916bdf41bd59135c366
imilen <m.ivanov@email.com>
2026-05-13 12:38:21 +0300

    build(root): narrow storybook story pattern

--- apps/root/.storybook/main.ts
+++ apps/root/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../src/app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  stories: ['../src/app/**/*.@(mdx|stories.ts)'],
   addons: [],
   framework: {
     name: '@storybook/angular',
